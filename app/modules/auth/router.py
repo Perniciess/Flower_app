3eb23ff46ff593e2cfcaffb3f3a6c18846615d0a
Perniciess <bajenov.valentin@gmail.com>
2026-01-23 07:50:29 +0300

    docs: add docstring to auth/router

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -13,14 +13,14 @@ from .utils import remove_token, set_token
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@auth_router.post("/register", response_model=UserResponse)
+@auth_router.post("/register", response_model=UserResponse, summary="Регистрация пользователя")
 async def register(data: AuthRegister, session: AsyncSession = Depends(get_db)) -> UserResponse:
     """Регистрация пользователя"""
     user = await auth_service.register(session=session, data=data)
     return user
 
 
-@auth_router.post("/login")
+@auth_router.post("/login", summary="Авторизация пользователя")
 async def login(response: Response, data: AuthLogin, session: AsyncSession = Depends(get_db)) -> dict[str, str]:
     """Авторизация пользователя"""
     tokens = await auth_service.login(session=session, data=data)
@@ -28,20 +28,23 @@ async def login(response: Response, data: AuthLogin, session: AsyncSession = Dep
     return {"message": "Успешная авторизация", "email": data.email}
 
 
-@auth_router.post("/logout")
+@auth_router.post("/logout", summary="Выход пользователя")
 async def logout(
     response: Response,
     refresh_token: str = Cookie(),
     session: AsyncSession = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ) -> dict[str, str]:
-    """Выход из системы"""
+    """
+    Выход из системы\n
+    Необходимо быть авторизованным в системе
+    """
     await auth_service.logout(session=session, refresh_token=refresh_token)
     remove_token(response)
     return {"message": "Выход из системы выполнен"}
 
 
-@auth_router.post("/refresh")
+@auth_router.post("/refresh", summary="Обновление токенов")
 async def refresh_token(response: Response, refresh_token: str = Cookie(), session: AsyncSession = Depends(get_db)) -> dict[str, str]:
     """Обновление токенов"""
     tokens = await auth_service.refresh_tokens(session=session, refresh_token=refresh_token)
