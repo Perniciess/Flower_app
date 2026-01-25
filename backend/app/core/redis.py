7a9b02b89d7aa11859eca4407d3b4263ec8b2aaf
Perniciess <bajenov.valentin@gmail.com>
2026-01-25 10:15:02 +0300

    docs: add docstring to security, cart_service, redis in back and bot

--- backend/app/core/redis.py
+++ backend/app/core/redis.py
@@ -4,21 +4,23 @@ from .config import settings
 
 
 class RedisManager:
+    """Класс для работы с Redis."""
+
     def __init__(self):
         self.pool: ConnectionPool | None = None
 
     async def init_pool(self) -> None:
-        """Инициализация пула. Вызывается при старте приложения."""
+        """Инициализация пула."""
         self.pool = ConnectionPool.from_url(f"{settings.REDIS_URL}", max_connections=10, decode_responses=True)
 
     async def close_pool(self) -> None:
-        """Закрытие пула. Вызывается при остановке приложения."""
+        """Закрытие пула."""
         if self.pool:
             await self.pool.disconnect()
             self.pool = None
 
     def get_client(self) -> Redis:
-        """Создает клиент из существующего пула."""
+        """Создаение клиента из существующего пула."""
         if self.pool is None:
             raise RuntimeError("Redis pool is not initialized. Call init_pool() first.")
         return Redis(connection_pool=self.pool)
--- bot/app/core/redis.py
+++ bot/app/core/redis.py
@@ -5,26 +5,32 @@ from .config import settings
 
 
 class RedisManager:
+    """Класс для работы с Redis."""
+
     def __init__(self):
         self.pool: ConnectionPool | None = None
         self.storage: RedisStorage | None = None
 
     async def init_pool(self) -> None:
+        """Инициализация пула."""
         self.pool = ConnectionPool.from_url(settings.REDIS_URL, max_connections=10, decode_responses=True)
         redis = Redis(connection_pool=self.pool)
         self.storage = RedisStorage(redis=redis, state_ttl=300, data_ttl=300)
 
     async def close_pool(self) -> None:
+        """Закрытие пула."""
         if self.pool:
             await self.pool.disconnect()
             self.pool = None
 
     def get_client(self) -> Redis:
+        """Создаение клиента из существующего пула."""
         if self.pool is None:
             raise RuntimeError("Redis pool not initialized")
         return Redis(connection_pool=self.pool)
 
     def get_storage(self) -> RedisStorage:
+        """Получение RedisStorage для FSM."""
         if self.storage is None:
             raise RuntimeError("Redis storage not initialized")
         return self.storage
