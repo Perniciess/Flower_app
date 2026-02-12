82f878163b1173483cb3e170974087354d1068c6
Perniciess <bajenov.valentin@gmail.com>
2026-02-12 23:55:31 +0300

    refactor: final new categorycard design

--- frontend/src/entities/category/ui/CategoryCard.tsx
+++ frontend/src/entities/category/ui/CategoryCard.tsx
@@ -6,7 +6,7 @@ export function CategoryCard({ category }: CategoryCardProps) {
     const apiUrl = "http://localhost:8000";
 
     return (
-        <div className="group relative flex h-75 w-78.75 flex-col items-start justify-end gap-2.5 overflow-hidden rounded-2xl p-[15px_12px]">
+        <div className="group relative flex h-75 w-78.75 flex-col items-start justify-end overflow-hidden rounded-2xl p-4">
             <div className="absolute inset-0 -z-10 transition-transform duration-300 ease-out group-hover:scale-105">
                 <Image
                     src={`${apiUrl}${imagePath}`}
@@ -17,11 +17,11 @@ export function CategoryCard({ category }: CategoryCardProps) {
                 />
             </div>
             <div className="absolute inset-0 bg-linear-to-b from-transparent to-black/20 -z-5" />
-            <p className="relative z-20 text-[28px] font-medium leading-[100%] tracking-normal text-white">
+            <p className="relative z-20 text-[28px] font-medium leading-[100%] tracking-normal text-white transition-transform duration-300 ease-out group-hover:translate-x-1 group-hover:-translate-y-2">
                 {category.name}
             </p>
             <div
-                className="pointer-events-none absolute inset-3 z-10 rounded-xl border border-white opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+                className="pointer-events-none absolute top-[11px] right-[11px] bottom-[11px] left-[11px] z-10 rounded-lg border border-white opacity-0 transition-opacity duration-200 group-hover:opacity-100"
                 aria-hidden
             />
         </div>
